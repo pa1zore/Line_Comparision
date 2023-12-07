46d57228add90914bc240ea22a2ad5c5ef22e2ec
pa1zore <pavanzore345@gmail.com>
2023-12-07 09:28:35 +0530

    Modified: Calculate line length

--- src/com/bridgelabz/Line_Comparison.java
+++ src/com/bridgelabz/Line_Comparison.java
@@ -3,5 +3,18 @@ package com.bridgelabz;
 public class Line_Comparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x1: ");
+        double x1 = sc.nextDouble();
+        System.out.print("Enter y1: ");
+        double y1 = sc.nextDouble();
+        System.out.print("Enter x2: ");
+        double x2 = sc.nextDouble();
+        System.out.print("Enter y2: ");
+        double y2 = sc.nextDouble();
+        double deltaX = x2 - x1;
+        double deltaY = y2 - y1;
+        double length = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        System.out.println("The length of the line is: " + length);
     }
 }
